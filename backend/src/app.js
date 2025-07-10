e91e535accbe2953dcfe659412bcaad773d4b86b
Shreya <shreyakumari44611@gmail.com>
2025-07-10 21:11:28 +0530

    add dashboard_url

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env'
+})
 
  
 
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.DASHBOARD_URL,
     methods:['GET','PUT','POST','DELETE'],
     credentials:true
 }))
