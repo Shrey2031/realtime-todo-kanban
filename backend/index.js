a361619335bb3b1f5746c4a033f20428a731f127
Shreya <shreyakumari44611@gmail.com>
2025-07-11 16:57:16 +0530

    add rate limit

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,16 @@ dotenv.config({
     path: './.env'
 })
 
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 60, // limit each IP to 60 requests per minute
+  message: "Too many requests, please try again later."
+});
+
+app.use(limiter); // apply to all requests
+
 const PORT = process.env.PORT || 9000; 
 
 const server = http.createServer(app); 
